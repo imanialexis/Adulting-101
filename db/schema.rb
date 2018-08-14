ce95ddae799c19620740d4859aa5d36ab84cb708
imani alexis <alexis.bailey37@gmail.com>
2018-08-14 15:32:26 -0400

    re created post_tag, it went missing for some reason

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_13_184531) do
+ActiveRecord::Schema.define(version: 2018_08_14_192720) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "post_tags", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "tag_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.integer "user_id"
