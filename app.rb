9085ac94b5ca189a8c8c8d54fbf9bb8915ebbfb3
imani alexis <alexis.bailey37@gmail.com>
2018-08-14 17:14:12 -0400

    added links for login and signout button

--- app.rb
+++ app.rb
@@ -6,12 +6,26 @@ require './models/post.rb'
 require './models/tag.rb'
 require './models/post_tag.rb'
 
-
+enable :sessions
 
 set :database, {adapter: 'postgresql', database: 'adulting'}
 
 get "/" do
 
-    erb :signup_or_login , :layout => :layouttwo
-
+    erb :homepage , :layout => :layouttwo
 end
+
+get "/login" do
+    
+    erb :login_page
+end
+
+
+
+# get "/" do
+#     if session[:user_id]
+#       erb :signed_in_homepage
+#     else
+#       erb :signed_out_homepage
+#     end
+#   end
